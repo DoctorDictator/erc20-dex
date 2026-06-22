974074597a2e4d8d93048dfc42d8be2bb97f8182
DoctorDictator <harshshishodia08@gmail.com>
2026-06-22 20:58:28 +0530

    chore: migrate Hardhat project to TypeScript

---
+++ hardhat.config.ts
@@ -0,0 +1,37 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ignition";
+import "@nomicfoundation/hardhat-ignition-ethers";
+import "solidity-coverage";
+import "hardhat-gas-reporter";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.27",
+    settings: {
+      optimizer: { enabled: true, runs: 200 },
+    },
+  },
+  networks: {
+    hardhat: {},
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || "",
+  },
+  gasReporter: {
+    enabled: process.env.GAS_REPORT === "true",
+    currency: "USD",
+  },
+};
+
+export default config;
